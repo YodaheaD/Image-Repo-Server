e2f140ff1bdfec0b22d66288fbfb5eb6c87f1755
YodaheaD <yosterdaniel@gmail.com>
2025-01-26 11:22:11 -0500

    protect conn string, add it to enc under AZURE_STORAGE_CONNECTION_STRING

--- src/scripts/moveToAzureStorage.ts
+++ src/scripts/moveToAzureStorage.ts
@@ -2,22 +2,30 @@ import { BlobServiceClient } from "@azure/storage-blob";
 
 import fs from "fs";
 import cliProgress from "cli-progress";
-import {  yodaheaBucket } from "../db/blobs";
+import { yodaheaBucket } from "../db/blobs";
 export async function migrateImages() {
+  const imagesStorage = await yodaheaBucket.listImages();
 
-    const imagesStorage = await yodaheaBucket.listImages();
-
-    const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-    progressBar.start(imagesStorage.length, 0);
-
-    for(let i = 0; i < imagesStorage.length; i++) {
-        const image = imagesStorage[i];
-        const imageBuffer = await yodaheaBucket.downloadBuffer(image);
-        const blobServiceClient = BlobServiceClient.fromConnectionString("DefaultEndpointsProtocol=https;AccountName=imagerepostorage;AccountKey=eHJQ7Fh7RkXjLy0qqvZSBAc2cFt2+UYWQKHjqWFQAdxQvSst22blBFUXaNi3kLX/vVvYAvLmlAwv+ASt9WICVQ==;EndpointSuffix=core.windows.net");
-        const containerClient = blobServiceClient.getContainerClient("familycon");
-        const blockBlobClient = containerClient.getBlockBlobClient(image);
-        await blockBlobClient.upload(imageBuffer, imageBuffer.byteLength);
-        progressBar.increment();
-    }
+  const progressBar = new cliProgress.SingleBar(
+    {},
+    cliProgress.Presets.shades_classic
+  );
+  progressBar.start(imagesStorage.length, 0);
+  const connStringForAzure = String(
+    process.env.AZURE_STORAGE_CONNECTION_STRING
+  );
+  if (!connStringForAzure) {
+    throw new Error("Azure Storage Connection String is not provided");
+  }
+  for (let i = 0; i < imagesStorage.length; i++) {
+    const image = imagesStorage[i];
+    const imageBuffer = await yodaheaBucket.downloadBuffer(image);
+    const blobServiceClient =
+      BlobServiceClient.fromConnectionString(connStringForAzure);
+    const containerClient = blobServiceClient.getContainerClient("familycon");
+    const blockBlobClient = containerClient.getBlockBlobClient(image);
+    await blockBlobClient.upload(imageBuffer, imageBuffer.byteLength);
+    progressBar.increment();
+  }
 }
 migrateImages();
